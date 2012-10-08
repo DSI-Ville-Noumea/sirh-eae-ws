2451f403641cf804e4bd199f205528155566300c
rayni84 <rayni84@ville-noumea.nc>
2012-10-08 21:27:46 +0000

    added baseDeDonnees.apt for tracking BDD use

--- src/main/java/nc/noumea/mairie/sirh/eae/web/controller/EaeController.java
+++ src/main/java/nc/noumea/mairie/sirh/eae/web/controller/EaeController.java
@@ -2,7 +2,6 @@ package nc.noumea.mairie.sirh.eae.web.controller;
 
 import java.util.List;
 
-import nc.noumea.mairie.sirh.domain.Agent;
 import nc.noumea.mairie.sirh.eae.domain.Eae;
 import nc.noumea.mairie.sirh.eae.service.IAgentMatriculeConverterService;
 import nc.noumea.mairie.sirh.eae.service.IEaeService;
@@ -17,8 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import flexjson.JSONSerializer;
 
 @RooWebJson(jsonObject = Eae.class)
 @Controller
@@ -49,24 +46,6 @@ public class EaeController {
 		String jsonResult = Eae.getSerializerForEaeList().serialize(result);
 		
 		return new ResponseEntity<String>(jsonResult, headers, HttpStatus.OK);
-	}
-	
-	@ResponseBody
-	@RequestMapping("findAgent")
-	public ResponseEntity<String> findAgent(@RequestParam("id") int id) {
-		
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.add("Content-Type", "application/json; charset=utf-8");
-	    Agent result = Agent.findAgent(id);
-	    
-	    if (result == null)
-	    	return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
-	    
-	    JSONSerializer serializer = new JSONSerializer()
-	    .exclude("class")
-	    .exclude("version");
-        
-        return new ResponseEntity<String>(serializer.serialize(result), headers, HttpStatus.OK);
 	}
 }
 	
