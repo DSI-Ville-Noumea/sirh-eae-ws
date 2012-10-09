1af4999d6b69a0679991b5c4a7d34fb9e2f797e8
rayni84 <rayni84@ville-noumea.nc>
2012-10-09 00:56:28 +0000

    added SQL scripts for alter/insert from previous version

---
+++ src/site/resources/doc/scripts/0.1.1.BUILD-SNAPSHOT-alter.sql
@@ -0,0 +1,22 @@
+alter table EAE_EVALUE add (VERSION number);
+alter table EAE add (VERSION number);
+alter table EAE drop column DIRECTION_SERVICE;
+alter table EAE drop column SERVICE;
+alter table EAE drop column SECTION_SERVICE;
+alter table EAE drop column ID_SHD;
+alter table EAE_DIPLOME add (VERSION number);
+alter table EAE_FICHE_POSTE drop column ID_AGENT;
+alter table EAE_FICHE_POSTE drop column NOM_RESP;
+alter table EAE_FICHE_POSTE drop column PRENOM_RESP;
+alter table EAE_FICHE_POSTE add (ID_SHD number);
+alter table EAE_FICHE_POSTE add (VERSION number);
+alter table EAE_FICHE_POSTE rename column DIRECTION_SERV to DIRECTION_SERVICE;
+alter table EAE_FICHE_POSTE rename column SERVICE_SERV to SERVICE;
+alter table EAE_FICHE_POSTE rename column SECTION_SERV to SECTION_SERVICE;
+RENAME EAE_FDP_ACTIVITES TO EAE_FDP_ACTIVITE;
+ALTER TABLE EAE_FDP_ACTIVITE rename column ID_EAE_FDP_ACTIVITES to ID_EAE_FDP_ACTIVITE;
+alter table EAE_FDP_ACTIVITE add (VERSION number);
+alter table EAE_PARCOURS_PRO add (VERSION number);
+alter table EAE_FORMATION add (VERSION number);
+alter table EAE_RESULTAT add (VERSION number);
+alter table EAE_RESULTAT rename column ID_TYPE_RESULTAT to ID_EAE_TYPE_RESULTAT
--- src/site/resources/doc/scripts/0.1.1.BUILD-SNAPSHOT.sql
+++
@@ -1,3 +0,0 @@
-alter table EAE_EVALUE add (VERSION number);
-alter table EAE add (VERSION number);
-alter table EAE_DIPLOME add (VERSION number);
