7c8297f21b84266656173258b019005063bc1520
rayni84 <rayni84@ville-noumea.nc>
2012-10-10 03:21:28 +0000

    update alter table commands to add "default 0 not null" to VERSION column

--- src/site/resources/doc/scripts/0.1.1-alter.sql
+++ src/site/resources/doc/scripts/0.1.1-alter.sql
@@ -1,24 +1,24 @@
 -- Connecte en EAE_ADM
-alter table EAE_EVALUE add (VERSION number);
-alter table EAE add (VERSION number);
+alter table EAE_EVALUE add (VERSION number default 0 not null);
+alter table EAE add (VERSION number default 0 not null);
 alter table EAE drop column DIRECTION_SERVICE;
 alter table EAE drop column SERVICE;
 alter table EAE drop column SECTION_SERVICE;
 alter table EAE drop column ID_SHD;
-alter table EAE_DIPLOME add (VERSION number);
+alter table EAE_DIPLOME add (VERSION number default 0 not null);
 alter table EAE_FICHE_POSTE drop column ID_AGENT;
 alter table EAE_FICHE_POSTE drop column NOM_RESP;
 alter table EAE_FICHE_POSTE drop column PRENOM_RESP;
 alter table EAE_FICHE_POSTE add (ID_SHD number);
-alter table EAE_FICHE_POSTE add (VERSION number);
+alter table EAE_FICHE_POSTE add (VERSION number default 0 not null);
 alter table EAE_FICHE_POSTE rename column DIRECTION_SERV to DIRECTION_SERVICE;
 alter table EAE_FICHE_POSTE rename column SERVICE_SERV to SERVICE;
 alter table EAE_FICHE_POSTE rename column SECTION_SERV to SECTION_SERVICE;
 alter table EAE_FDP_ACTIVITE rename column ID_EAE_FDP_ACTIVITES to ID_EAE_FDP_ACTIVITE;
-alter table EAE_FDP_ACTIVITE add (VERSION number);
-alter table EAE_PARCOURS_PRO add (VERSION number);
-alter table EAE_FORMATION add (VERSION number);
-alter table EAE_RESULTAT add (VERSION number);
+alter table EAE_FDP_ACTIVITE add (VERSION number default 0 not null);
+alter table EAE_PARCOURS_PRO add (VERSION number default 0 not null);
+alter table EAE_FORMATION add (VERSION number default 0 not null);
+alter table EAE_RESULTAT add (VERSION number default 0 not null);
 alter table EAE_RESULTAT rename column ID_TYPE_RESULTAT to ID_EAE_TYPE_RESULTAT;
 alter table EAE_NIVEAU rename column ID_EAE_NIVEAU_EAE to ID_EAE_NIVEAU;
 
