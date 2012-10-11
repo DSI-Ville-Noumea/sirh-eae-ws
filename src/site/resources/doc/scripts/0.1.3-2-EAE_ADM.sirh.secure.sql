e222dc294d751ce64a03c75d35f26ce46aea8fa9
salad74 <salad74@ville-noumea.nc>
2012-10-11 23:41:31 +0000

    Scripts de mise en recette qui permettra à l'application SIRH d'interagir de manière sécurisée avec les données dee EAE. Cette configuration devra être intégrée dans une datasource dédié à l'application SIRH, via le user SIRH_USR.

---
+++ src/site/resources/doc/scripts/0.1.3-2-EAE_ADM.sirh.secure.sql
@@ -0,0 +1,15 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- connecte en EAE_ADM
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+grant select, insert, delete on EAE_EVALUE to SIRH_USR;
+grant select, insert, update, delete on  EAE_CAMPAGNE_EAE to SIRH_USR;
+grant select, insert, update, delete on  EAE_DOCUMENT to SIRH_USR;
+grant select, insert, update, delete on  EAE_CAMPAGNE_ACTION to SIRH_USR;
+grant select, insert, update, delete on  EAE_CAMPAGNE_ACTEURS to SIRH_USR;
+grant select, insert, delete on  EAE_EVALUATEUR to SIRH_USR;
+grant select, insert, delete on  EAE_FICHE_POSTE to SIRH_USR;
+grant select, insert, delete on  EAE_FDP_ACTIVITE to SIRH_USR;
+grant select, insert, delete on  EAE to SIRH_USR;
+grant select, insert, delete on  EAE_DIPLOME to SIRH_USR;
+grant select, insert, delete on  EAE_PARCOURS_PRO to SIRH_USR;
+grant select, insert, delete on  EAE_FORMATION to SIRH_USR;
---
+++ src/site/resources/doc/scripts/0.1.3-3-EAE_USR.sirh.test.sql
@@ -0,0 +1,15 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- connecte en EAE_USR
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+select * from EAE_EVALUE;
+select * from EAE_CAMPAGNE_EAE;
+select * from EAE_DOCUMENT;
+select * from EAE_CAMPAGNE_ACTION;
+select * from EAE_CAMPAGNE_ACTEURS;
+select * from EAE_EVALUATEUR;
+select * from EAE_FICHE_POSTE;
+select * from EAE_FDP_ACTIVITE;
+select * from EAE;
+select * from EAE_DIPLOME;
+select * from EAE_PARCOURS_PRO;
+select * from EAE_FORMATION;
