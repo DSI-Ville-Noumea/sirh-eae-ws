813b48d38222a6d228d740c0ca6dd0af236df42f
rayni84 <rayni84@ville-noumea.nc>
2012-10-11 23:45:28 +0000

    fixed grant scripts/0.1.3-2-EAE_ADM.sirh.secure.sql to use role instead of user

--- src/site/resources/doc/scripts/0.1.3-2-EAE_ADM.sirh.secure.sql
+++ src/site/resources/doc/scripts/0.1.3-2-EAE_ADM.sirh.secure.sql
@@ -1,15 +1,15 @@
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 -- connecte en EAE_ADM
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-grant select, insert, delete on EAE_EVALUE to SIRH_USR;
-grant select, insert, update, delete on  EAE_CAMPAGNE_EAE to SIRH_USR;
-grant select, insert, update, delete on  EAE_DOCUMENT to SIRH_USR;
-grant select, insert, update, delete on  EAE_CAMPAGNE_ACTION to SIRH_USR;
-grant select, insert, update, delete on  EAE_CAMPAGNE_ACTEURS to SIRH_USR;
-grant select, insert, delete on  EAE_EVALUATEUR to SIRH_USR;
-grant select, insert, delete on  EAE_FICHE_POSTE to SIRH_USR;
-grant select, insert, delete on  EAE_FDP_ACTIVITE to SIRH_USR;
-grant select, insert, delete on  EAE to SIRH_USR;
-grant select, insert, delete on  EAE_DIPLOME to SIRH_USR;
-grant select, insert, delete on  EAE_PARCOURS_PRO to SIRH_USR;
-grant select, insert, delete on  EAE_FORMATION to SIRH_USR;
+grant select, insert, delete on EAE_EVALUE to R_SIRH_USR;
+grant select, insert, update, delete on  EAE_CAMPAGNE_EAE to R_SIRH_USR;
+grant select, insert, update, delete on  EAE_DOCUMENT to R_SIRH_USR;
+grant select, insert, update, delete on  EAE_CAMPAGNE_ACTION to R_SIRH_USR;
+grant select, insert, update, delete on  EAE_CAMPAGNE_ACTEURS to R_SIRH_USR;
+grant select, insert, delete on  EAE_EVALUATEUR to R_SIRH_USR;
+grant select, insert, delete on  EAE_FICHE_POSTE to R_SIRH_USR;
+grant select, insert, delete on  EAE_FDP_ACTIVITE to R_SIRH_USR;
+grant select, insert, delete on  EAE to R_SIRH_USR;
+grant select, insert, delete on  EAE_DIPLOME to R_SIRH_USR;
+grant select, insert, delete on  EAE_PARCOURS_PRO to R_SIRH_USR;
+grant select, insert, delete on  EAE_FORMATION to R_SIRH_USR;
