808f3091ebf8f14e83cd5aae6f27f9ac047b7038
salad74 <salad74@ville-noumea.nc>
2012-10-15 23:00:06 +0000

    Correction suite à un problème de droit d'accès en recette, sur les séquences.

--- src/site/resources/doc/scripts/0.1.4-5-EAE_ADM.grants.select.sql
+++ src/site/resources/doc/scripts/0.1.4-5-EAE_ADM.grants.select.sql
@@ -3,3 +3,9 @@
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 sho user
 grant select on EAE to R_SIRH_WS_RO;
+
+grant select on EAE_S_EAE to R_SIRH_WS_RO;
+grant select on EAE_S_CAMPAGNE_EAE to R_SIRH_WS_RO;
+grant select on EAE_S_EVALUATEUR to R_SIRH_WS_RO;
+grant select on EAE_S_EVALUATION to R_SIRH_WS_RO;
+grant select on EAE_S_FICHE_POSTE to R_SIRH_WS_RO;
