0cd679fd4ed19b559909b8f2fa4c26821a3226da
rayni84 <rayni84@ville-noumea.nc>
2012-10-23 21:56:27 +0000

    added scripts/0.1.5-4-EAE_ADM.comment.sql for adding Oracle comments to the EAE tables cf. http://dvlp:8181/browse/EAEWS-5

---
+++ src/site/resources/doc/scripts/0.1.5-4-EAE_ADM.comment.sql
@@ -0,0 +1,28 @@
+----------------------------------------------------------------
+-- Connecte en EAE_ADM
+----------------------------------------------------------------
+comment on table EAE_FDP_COMPETENCE is 'La liste des compétences requises pour une fiche de poste';
+comment on table EAE_COMMENTAIRE is 'Commentaires sur les différentes parties d''un EAE';
+comment on table EAE_NIVEAU is 'Le référentiel des différents niveaux de résultats';
+comment on table EAE_FDP_ACTIVITE is 'La liste des activités principales liées à une fiche de pose';
+comment on table EAE_CAMPAGNE_EAE is 'SIRH: Campagne regroupant tous les EAEs pour une année donnée';
+comment on table EAE_CAMPAGNE_ACTION is 'SIRH: Liste d''actions à entreprendre durant une campagne EAE';
+comment on table EAE_CAMPAGNE_ACTEURS is 'SIRH: Liste des acteurs de la campagne EAE';
+comment on table EAE is 'Informations générales sur l''EAE';
+comment on table EAE_EVALUATEUR is 'Une personne ayant droit d''évaluer un agent';
+comment on table EAE_EVALUE is 'L''agent évalué dans le cadre de son EAE';
+comment on table EAE_FICHE_POSTE is 'Fiche de poste décrivant le travail et les compétences requises de l''agent actuellement';
+comment on table EAE_DIPLOME is 'La liste des dîplomes de l''agent évalué';
+comment on table EAE_PARCOURS_PRO is 'La liste des expériences et parcours professionnel de l''agent évalué';
+comment on table EAE_FORMATION is 'La liste des formations auxquelles l''agent évalué a participé';
+comment on table EAE_RESULTAT is 'Le résultat de l''évaluation des différents objectifs de l''agent évalué';
+comment on table EAE_PLAN_ACTION is 'La liste des objectifs à réaliser pour l''année suivante. (Deviendront le résultat du prochain EAE)';
+comment on table EAE_EVALUATION is 'L''évaluation de l''agent en regard de ses objectifs et performance';
+comment on table EAE_DOCUMENT is 'SIRH: Liste des documents liés à une campagne EAE';
+comment on table EAE_APPRECIATION is 'La liste des appréciations d''un agent par son évaluateur et par lui même';
+comment on table EAE_AUTO_EVALUATION is 'L''auto évaluation d''un agent';
+comment on table EAE_EVOLUTION is 'L''évolution d''un agent dans son travail vu par l''agent';
+comment on table EAE_EVOL_SOUHAIT is 'Les souhaits d''évolution d''un agent dans ses compétences et ses tâches';
+comment on table EAE_TYPE_DEVELOPPEMENT is 'Le référentiel des différents type de développements de l''agent';
+comment on table EAE_DEVELOPPEMENT is 'La liste des développements de l''agent planifiés pour l''année suivante';
+comment on table EAE_TYPE_OBJECTIF is 'Le référentiel des types d''objectifs d''un agent';
