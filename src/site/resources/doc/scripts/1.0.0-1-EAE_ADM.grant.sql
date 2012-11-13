7ffc12970cca057502e9f389d082966a8595e0f3
rayni84 <rayni84@ville-noumea.nc>
2012-11-13 05:20:55 +0000

    added scripts/1.0.0-1-EAE_ADM.grant.sql for passing 1.0.0 version

---
+++ src/site/resources/doc/scripts/1.0.0-1-EAE_ADM.grant.sql
@@ -0,0 +1,6 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- connecte en EAE_ADM
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON EAE_FDP_COMPETENCE TO R_SIRH_USR;
+GRANT SELECT ON EAE_S_FDP_COMPETENCE TO R_SIRH_USR;
