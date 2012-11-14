b7bba2e3212729a64663ab2393ebc1f571e2536b
rayni84 <rayni84@ville-noumea.nc>
2012-11-14 20:57:25 +0000

    updated scripts/1.0.0-2-EAE_USR.delete.sql

---
+++ src/site/resources/doc/scripts/1.0.0-2-EAE_USR.delete.sql
@@ -0,0 +1,17 @@
+delete from EAE_APPRECIATION;
+delete from EAE_AUTO_EVALUATION;
+delete from EAE_EVALUATION;
+delete from EAE_FDP_COMPETENCE;
+delete from EAE_FDP_ACTIVITE;
+delete from EAE_FICHE_POSTE;
+delete from EAE_PARCOURS_PRO;
+delete from EAE_PLAN_ACTION;
+delete from EAE_RESULTAT;
+delete from EAE_DIPLOME;
+delete from EAE_EVALUATEUR;
+delete from EAE_EVALUE;
+delete from EAE;
+delete from EAE_CAMPAGNE_ACTEURS;
+delete from EAE_CAMPAGNE_ACTION;
+delete from EAE_CAMPAGNE_EAE;
+delete from EAE_COMMENTAIRE;
