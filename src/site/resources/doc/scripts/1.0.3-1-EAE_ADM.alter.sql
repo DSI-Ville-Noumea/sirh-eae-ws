e2bbcb07eaefa12d963685c0f1b97fc83cde6362
rayni84 <rayni84@ville-noumea.nc>
2012-12-05 05:28:39 +0000

    sirh-eae-ws-1.0.3

--- src/site/resources/doc/scripts/1.0.3-1-EAE_ADM.alter.sql
+++ src/site/resources/doc/scripts/1.0.3-1-EAE_ADM.alter.sql
@@ -8,3 +8,9 @@ alter table EAE_FORMATION add DUREE_FORMATION_NEW varchar2(50);
 update EAE_FORMATION set DUREE_FORMATION_NEW = DUREE_FORMATION;
 alter table EAE_FORMATION drop column DUREE_FORMATION;
 alter table EAE_FORMATION rename column DUREE_FORMATION_NEW to DUREE_FORMATION;
+
+alter table EAE_CAMPAGNE_ACTION add message_new clob;
+update EAE_CAMPAGNE_ACTION set message_new = message;
+alter table EAE_CAMPAGNE_ACTION drop column message;
+alter table EAE_CAMPAGNE_ACTION rename column message_new to message;
+alter table EAE_CAMPAGNE_ACTION drop column DIFFUSE;
