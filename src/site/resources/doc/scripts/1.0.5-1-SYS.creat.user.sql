3e102e940bece811995f555a38b602d7a348e356
rayni84 <rayni84@ville-noumea.nc>
2012-12-14 00:45:58 +0000

    update delivery information for 1.0.5

--- src/site/resources/doc/scripts/1.0.5-1-SYS.creat.user.sql
+++ src/site/resources/doc/scripts/1.0.5-1-SYS.creat.user.sql
@@ -2,9 +2,9 @@
 -- connecte en SYSTEM ou SYS
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-create role R_EAE_QRTZ_USR;
-grant connect, create session to R_EAE_QRTZ_USR;
-create user EAE_QRTZ_USR identified by PASSWORD_SECRET_SIE;
-grant R_EAE_QRTZ_USR to EAE_QRTZ_USR;
+create role R_EAE_JOBS_USR;
+grant connect, create session to R_EAE_JOBS_USR;
+create user EAE_JOBS_USR identified by PASSWORD_SECRET_SIE;
+grant R_EAE_JOBS_USR to EAE_JOBS_USR;
 
-alter user EAE_QRTZ_USR default tablespace TS_QRTZ_DEFAULT;
+alter user EAE_JOBS_USR default tablespace TS_QRTZ_DEFAULT;
