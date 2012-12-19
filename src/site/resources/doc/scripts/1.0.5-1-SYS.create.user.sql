f61e3b2bbca0973e65cb9a2955a429e53a48fcdd
rayni84 <rayni84@ville-noumea.nc>
2012-12-19 03:08:43 +0000

    sirh-eae-ws-1.0.5

--- src/site/resources/doc/scripts/1.0.5-1-SYS.create.user.sql
+++ src/site/resources/doc/scripts/1.0.5-1-SYS.create.user.sql
@@ -4,7 +4,10 @@
 
 create role R_EAE_JOBS_USR;
 grant connect, create session to R_EAE_JOBS_USR;
+
+-- remplacer avec un vrai mot de passe
 create user EAE_JOBS_USR identified by PASSWORD_SECRET_SIE;
+
 grant R_EAE_JOBS_USR to EAE_JOBS_USR;
 
-alter user EAE_JOBS_USR default tablespace TS_QRTZ_DEFAULT;
+alter user EAE_JOBS_USR default tablespace TS_EAE_DEFAULT;
