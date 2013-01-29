c85d4424c3b3265c67abe87603493cf979078afa
rayni84 <rayni84@ville-noumea.nc>
2013-01-29 22:08:18 +0000

    sirh-eae-ws-1.1.1

---
+++ src/site/resources/doc/scripts/1.1.1-1-EAE_ADM.grant.sql
@@ -0,0 +1,7 @@
+----------------------------------------------------------------
+-- Connecte en EAE_ADM
+----------------------------------------------------------------
+
+GRANT UPDATE ON EAE_ADM.EAE_EVALUE TO R_SIRH_WS_RO;
+GRANT UPDATE ON EAE_ADM.EAE_FICHE_POSTE TO R_SIRH_WS_RO;
+COMMIT;
