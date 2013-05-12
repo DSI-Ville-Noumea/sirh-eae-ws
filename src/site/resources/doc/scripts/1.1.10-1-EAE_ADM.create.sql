71c4b720a4a55569209271248268b00872e49f29
nicno85 <nicno85@ville-noumea.nc>
2013-05-12 21:32:45 +0000

    MER 1.1.10

---
+++ src/site/resources/doc/scripts/1.1.10-1-EAE_ADM.create.sql
@@ -0,0 +1,9 @@
+----------------------------------------------------------------
+-- Connecte en EAE_ADM
+----------------------------------------------------------------
+
+create sequence EAE_S_NUM_INCREMENT_DOCUMENT
+start with 1
+increment by 1
+nomaxvalue;
+
---
+++ src/site/resources/doc/scripts/1.1.10-2-EAE_ADM.grant.sql
@@ -0,0 +1,6 @@
+----------------------------------------------------------------
+-- Connecte en EAE_ADM
+----------------------------------------------------------------
+
+GRANT SELECT ON EAE_ADM.EAE_S_NUM_INCREMENT_DOCUMENT TO R_SIRH_USR;
+
