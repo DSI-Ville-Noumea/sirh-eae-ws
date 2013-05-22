ccd6bcb64bf353ff0bd17f827d24670a7d17c5e3
nicno85 <nicno85@ville-noumea.nc>
2013-05-22 04:54:34 +0000

    MER du 23/05/13

---
+++ src/site/resources/doc/scripts/1.1.11-1-EAE_ADM.grant.sql
@@ -0,0 +1,10 @@
+----------------------------------------------------------------
+-- Connecte en EAE_ADM
+----------------------------------------------------------------
+
+GRANT UPDATE ON EAE_ADM.EAE_S_NUM_INCREMENT_DOCUMENT TO R_SIRH_USR;
+GRANT SELECT ON EAE_ADM.EAE_S_EVOLUTION TO R_SIRH_USR;
+GRANT INSERT ON EAE_ADM.EAE_EVOLUTION TO R_SIRH_USR;
+
+
+
