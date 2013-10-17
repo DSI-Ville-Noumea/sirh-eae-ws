6f14666738f06a06e7971281fec3097f73ed1800
nicno85 <nicno85@ville-noumea.nc>
2013-10-17 04:15:47 +0000

    #4385 : augmentation de la taille du champ dans les appreciations.

---
+++ src/site/resources/doc/scripts/2.3-1-EAE_ADM.alter.sql
@@ -0,0 +1,6 @@
+----------------------------------------------------------------
+-- Connecte en EAE_ADM
+----------------------------------------------------------------
+
+ALTER TABLE EAE_APPRECIATION modify (NOTE_EVALUE varchar2(2 CHAR));
+ALTER TABLE EAE_APPRECIATION modify (NOTE_EVALUATEUR varchar2(2 CHAR));
