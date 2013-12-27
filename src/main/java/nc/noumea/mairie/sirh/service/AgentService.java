c5db308b57272e0a9622f304e6cce754253bbe20
nicno85 <nicno85@ville-noumea.nc>
2013-12-27 00:07:03 +0000

    #5202 : refactoring TU suite spring 4

--- src/main/java/nc/noumea/mairie/sirh/service/AgentService.java
+++ src/main/java/nc/noumea/mairie/sirh/service/AgentService.java
@@ -1,5 +1,8 @@
 package nc.noumea.mairie.sirh.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import nc.noumea.mairie.sirh.domain.Agent;
 import nc.noumea.mairie.sirh.eae.domain.Eae;
 import nc.noumea.mairie.sirh.eae.domain.EaeEvaluateur;
@@ -11,55 +14,58 @@ import org.springframework.stereotype.Service;
 @Service
 public class AgentService implements IAgentService {
 
+	@PersistenceContext(unitName = "eaePersistenceUnit")
+	private EntityManager eaeEntityManager;
+
 	@Override
 	public Eae fillEaeWithAgents(Eae eaeToFill) {
 
 		if (eaeToFill.getEaeEvalue() != null)
 			fillEaeEvalueWithAgent(eaeToFill.getEaeEvalue());
-		
+
 		if (eaeToFill.getEaeFichePostes() != null)
 			for (EaeFichePoste fdp : eaeToFill.getEaeFichePostes())
 				fillEaeFichePosteWithAgent(fdp);
-		
+
 		if (eaeToFill.getIdAgentDelegataire() != null)
 			eaeToFill.setAgentDelegataire(getAgent(eaeToFill.getIdAgentDelegataire()));
-		
+
 		if (eaeToFill.getEaeEvaluateurs() != null)
 			for (EaeEvaluateur eval : eaeToFill.getEaeEvaluateurs())
 				fillEaeEvaluateurWithAgent(eval);
-		
+
 		return eaeToFill;
 	}
-	
+
 	@Override
 	public EaeEvalue fillEaeEvalueWithAgent(EaeEvalue eaeEvalueToFill) {
 
 		Agent agent = getAgent(eaeEvalueToFill.getIdAgent());
 		eaeEvalueToFill.setAgent(agent);
-		
+
 		return eaeEvalueToFill;
 	}
 
 	@Override
 	public EaeEvaluateur fillEaeEvaluateurWithAgent(EaeEvaluateur eaeEvaluateurToFill) {
-		
+
 		Agent agent = getAgent(eaeEvaluateurToFill.getIdAgent());
 		eaeEvaluateurToFill.setAgent(agent);
-		
+
 		return eaeEvaluateurToFill;
 	}
-	
+
 	@Override
 	public EaeFichePoste fillEaeFichePosteWithAgent(EaeFichePoste eaeFichePosteToFill) {
-		
+
 		Agent agent = getAgent(eaeFichePosteToFill.getIdAgentShd());
 		eaeFichePosteToFill.setAgentShd(agent);
-		
+
 		return eaeFichePosteToFill;
 	}
 
 	@Override
 	public Agent getAgent(Integer idAgent) {
-		return Agent.findAgent(idAgent);
+		return eaeEntityManager.find(Agent.class, idAgent);
 	}
 }
--- src/test/java/nc/noumea/mairie/sirh/service/AgentServiceTest.java
+++ src/test/java/nc/noumea/mairie/sirh/service/AgentServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.EntityManager;
+
 import nc.noumea.mairie.sirh.domain.Agent;
 import nc.noumea.mairie.sirh.eae.domain.Eae;
 import nc.noumea.mairie.sirh.eae.domain.EaeEvaluateur;
@@ -12,8 +14,10 @@ import nc.noumea.mairie.sirh.eae.domain.EaeEvalue;
 import nc.noumea.mairie.sirh.eae.domain.EaeFichePoste;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.mock.staticmock.AnnotationDrivenStaticEntityMockingControl;
 import org.springframework.mock.staticmock.MockStaticEntityMethods;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @MockStaticEntityMethods
 public class AgentServiceTest {
@@ -30,18 +34,19 @@ public class AgentServiceTest {
 		agentToReturn.setIdAgent(998);
 		agentToReturn.setNomPatronymique("Bilbo");
 
-		Agent.findAgent(998);
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentToReturn);
-		AnnotationDrivenStaticEntityMockingControl.playback();
+		EntityManager emMock = Mockito.mock(EntityManager.class);
+		Mockito.when(emMock.find(Agent.class, 998)).thenReturn(agentToReturn);
+
+		AgentService service = new AgentService();
+		ReflectionTestUtils.setField(service, "eaeEntityManager", emMock);
 
 		// When
-		AgentService service = new AgentService();
 		service.fillEaeEvaluateurWithAgent(eval);
 
 		// Then
 		assertEquals(agentToReturn, eval.getAgent());
 	}
-	
+
 	@Test
 	public void testFillAgentForEaeFichePoste() {
 
@@ -54,18 +59,19 @@ public class AgentServiceTest {
 		agentToReturn.setIdAgent(998);
 		agentToReturn.setNomPatronymique("Bilbo");
 
-		Agent.findAgent(998);
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentToReturn);
-		AnnotationDrivenStaticEntityMockingControl.playback();
+		EntityManager emMock = Mockito.mock(EntityManager.class);
+		Mockito.when(emMock.find(Agent.class, 998)).thenReturn(agentToReturn);
+
+		AgentService service = new AgentService();
+		ReflectionTestUtils.setField(service, "eaeEntityManager", emMock);
 
 		// When
-		AgentService service = new AgentService();
 		service.fillEaeFichePosteWithAgent(fdp);
 
 		// Then
 		assertEquals(agentToReturn, fdp.getAgentShd());
 	}
-	
+
 	@Test
 	public void testFillAgentForEaeEvalue() {
 
@@ -78,12 +84,13 @@ public class AgentServiceTest {
 		agentToReturn.setIdAgent(995);
 		agentToReturn.setNomPatronymique("Billy");
 
-		Agent.findAgent(995);
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentToReturn);
-		AnnotationDrivenStaticEntityMockingControl.playback();
+		EntityManager emMock = Mockito.mock(EntityManager.class);
+		Mockito.when(emMock.find(Agent.class, 995)).thenReturn(agentToReturn);
+
+		AgentService service = new AgentService();
+		ReflectionTestUtils.setField(service, "eaeEntityManager", emMock);
 
 		// When
-		AgentService service = new AgentService();
 		service.fillEaeEvalueWithAgent(evalue);
 
 		// Then
@@ -105,23 +112,17 @@ public class AgentServiceTest {
 		EaeFichePoste fdp = new EaeFichePoste();
 		fdp.setIdAgentShd(idAgentShd);
 		eaeToReturn.getEaeFichePostes().add(fdp);
-		
+
 		// Mock the Agent find static method to return our agent
 		Agent agentToReturn = new Agent();
 		agentToReturn.setIdAgent(idAgent);
 		agentToReturn.setNomPatronymique("Bilbo");
 
-		Agent.findAgent(idAgent);
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentToReturn);
-		
 		// Mock the Agent static method to return our agentShd
 		Agent agentShdToReturn = new Agent();
 		agentShdToReturn.setIdAgent(idAgentShd);
 		agentShdToReturn.setNomPatronymique("yet another person shd");
 
-		Agent.findAgent(fdp.getIdAgentShd());
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentShdToReturn);
-				
 		// Mock the Agent static method to return our agentDelegataire
 		Agent agentDelegataireToReturn = new Agent();
 		agentDelegataireToReturn.setIdAgent(idAgentDelegataire);
@@ -129,12 +130,17 @@ public class AgentServiceTest {
 
 		Agent.findAgent(eaeToReturn.getIdAgentDelegataire());
 		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentDelegataireToReturn);
-						
+
 		AnnotationDrivenStaticEntityMockingControl.playback();
 
 		// Set the mock as the entityManager of the service class
+		EntityManager emMock = Mockito.mock(EntityManager.class);
+		Mockito.when(emMock.find(Agent.class, fdp.getIdAgentShd())).thenReturn(agentShdToReturn);
+		Mockito.when(emMock.find(Agent.class, idAgent)).thenReturn(agentToReturn);
+
 		AgentService service = new AgentService();
-		
+		ReflectionTestUtils.setField(service, "eaeEntityManager", emMock);
+
 		// When
 		Eae result = service.fillEaeWithAgents(eaeToReturn);
 
@@ -153,33 +159,29 @@ public class AgentServiceTest {
 		EaeEvalue evalue = new EaeEvalue();
 		evalue.setIdAgent(idAgent);
 		eaeToReturn.setEaeEvalue(evalue);
-		
+
 		Set<EaeEvaluateur> evals = new HashSet<EaeEvaluateur>();
 		EaeEvaluateur eval1 = new EaeEvaluateur();
 		eval1.setIdAgent(56);
 		evals.add(eval1);
 		eaeToReturn.setEaeEvaluateurs(evals);
-		
+
 		// Mock the Agent find static method to return our agent
 		Agent agentToReturn = new Agent();
 		agentToReturn.setIdAgent(evalue.getIdAgent());
 		agentToReturn.setNomPatronymique("Bilbo");
 
-		Agent.findAgent(evalue.getIdAgent());
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentToReturn);
-		
 		Agent agentToReturn2 = new Agent();
 		agentToReturn2.setIdAgent(eval1.getIdAgent());
 		agentToReturn2.setNomPatronymique("Bilbo2");
 
-		Agent.findAgent(eval1.getIdAgent());
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentToReturn2);
-				
-		AnnotationDrivenStaticEntityMockingControl.playback();
+		EntityManager emMock = Mockito.mock(EntityManager.class);
+		Mockito.when(emMock.find(Agent.class, evalue.getIdAgent())).thenReturn(agentToReturn);
+		Mockito.when(emMock.find(Agent.class, eval1.getIdAgent())).thenReturn(agentToReturn2);
 
-		// Set the mock as the entityManager of the service class
 		AgentService service = new AgentService();
-		
+		ReflectionTestUtils.setField(service, "eaeEntityManager", emMock);
+
 		// When
 		Eae result = service.fillEaeWithAgents(eaeToReturn);
 
@@ -187,26 +189,25 @@ public class AgentServiceTest {
 		assertEquals(agentToReturn, result.getEaeEvalue().getAgent());
 		assertEquals(1, result.getEaeEvaluateurs().size());
 		assertEquals(agentToReturn2, result.getEaeEvaluateurs().iterator().next().getAgent());
-		
+
 	}
-	
+
 	@Test
 	public void testGetAgent_callAgentStaticFinder() {
-		
+
 		// Given
 		Agent agentToReturn = new Agent();
 		agentToReturn.setIdAgent(789);
-		
-		// Setup mock for static Agent finder method
-		Agent.findAgent(789);
-		AnnotationDrivenStaticEntityMockingControl.expectReturn(agentToReturn);
-		AnnotationDrivenStaticEntityMockingControl.playback();
-		
+
+		EntityManager emMock = Mockito.mock(EntityManager.class);
+		Mockito.when(emMock.find(Agent.class, 789)).thenReturn(agentToReturn);
+
 		AgentService service = new AgentService();
-		
+		ReflectionTestUtils.setField(service, "eaeEntityManager", emMock);
+
 		// When
 		Agent result = service.getAgent(789);
-		
+
 		// Then
 		assertEquals(agentToReturn, result);
 	}
